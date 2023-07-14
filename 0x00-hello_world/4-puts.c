cba7bc990d47a82431fea0c4aad7877a0578dc74
Ponego16 <mohlapateka@gmail.com>
2023-07-13 18:44:19 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-int main(void) {
-puts("Programming is like building a multilingual puzzle");
-return (0);
-
- }
+/**
+ * main - Entry point
+ *
+ * return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
